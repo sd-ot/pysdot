6e3c8ec7fdd9e98ee2ed40375fa7ebb9cabea8de
Hugo Leclerc <hugal.leclerc@gmail.com>
2020-10-02 07:05:09 +0200

    choice of precision of approx for radial func

--- pysdot/radial_funcs/RadialFuncArfd.py
+++ pysdot/radial_funcs/RadialFuncArfd.py
@@ -2,8 +2,8 @@ import pybind_sdot_Arfd
 
 #
 class RadialFuncArfd:
-    def __init__(self, values, inp_scaling, out_scaling, der_values, der_inp_scaling, der_out_scaling, stops):
-        self.func = pybind_sdot_Arfd.Arfd(values, inp_scaling, out_scaling, der_values, der_inp_scaling, der_out_scaling, stops)
+    def __init__(self, values, inp_scaling, out_scaling, der_values, der_inp_scaling, der_out_scaling, stops, prec=1e-7):
+        self.func = pybind_sdot_Arfd.Arfd(values, inp_scaling, out_scaling, der_values, der_inp_scaling, der_out_scaling, stops, prec)
 
     def name(self):
         return self.func
--- tests/test_Arfd.py
+++ tests/test_Arfd.py
@@ -89,7 +89,8 @@ class TestArfd_2D_1p5(unittest.TestCase):
             lambda w: 1 / w ** 0.5, # input scaling for the der wrt weight
             lambda w: w ** 1.5, # output scaling for the der wrt weight
 
-            [ 1 ] # stops (radii value where we may have discontinuities)
+            [ 1 ], # stops (radii values where we may have discontinuities)
+            1e-8 # precision
         )
 
         # should use only 2 polynomials
@@ -109,32 +110,34 @@ class TestArfd_2D_1p5(unittest.TestCase):
         l = [
             # { "w": 0.5, "i": np.pi / 32, "c": [ 0 ] * 2 },
             # { "w":   1, "i": np.pi /  8, "c": [ 0 ] * 2 },
-            { "w":  10.0, "i":    50 /  3, "c": [ 0 ]     },
-            { "w": 100.0, "i":   590 /  3, "c": [ 0 ]     },
+            { "w":  10.0, "i":     417.04, "c": [ 0.815858, 0.476057 ]     },
+            { "w": 100.0, "i":  191827   , "c": [ 0.983124, 0.497884 ]     },
         ]
 
-        # Integrate[ Integrate[ ( 10 - ( x * x + y * y ) ) ^ 1.5, { x, 0, 2 } ], { y, 0, 1 } ] => 48.5122
-        # Integrate[ Integrate[ ( 100 - ( x * x + y * y ) ) ^ 1.5, { x, 0, 2 } ], { y, 0, 1 } ] => 1950.32
-
-        # Integrate[ Integrate[ ( 10 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ] => 417.04
+        # Integrate[ Integrate[ (  10 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ] => 417.04
+        # Integrate[ Integrate[ (  20 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ] => 2902.38
+        # Integrate[ Integrate[ (  40 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ] => 18230
+        # Integrate[ Integrate[ (  60 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ] => 52021
         # Integrate[ Integrate[ ( 100 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ] => 191827
+
+        # Integrate[ Integrate[ x * (  10 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ] / Integrate[ Integrate[ (  10 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ]
+        # Integrate[ Integrate[ y * (  10 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ] / Integrate[ Integrate[ (  10 - ( x * x + y * y ) ) ^ 2.5, { x, 0, 2 } ], { y, 0, 1 } ]
 
         # test integrals and centroids
         for d in l:
             self.pd.set_weights(np.array([d["w"]]))
 
             ig = self.pd.integrals()
-            print( ig )
-            # self.assertAlmostEqual(ig[ 0 ], d["i"])
+            self.assertAlmostEqual(ig[ 0 ], d["i"], delta=1)
 
-            # cs = self.pd.centroids()
-            # for (v,e) in zip( cs[0], d["c"] ):
-            #     self.assertAlmostEqual(v, e)
+            cs = self.pd.centroids()
+            for (v,e) in zip( cs[0], d["c"] ):
+                self.assertAlmostEqual(v, e, delta=1e-3)
 
-    # def test_derivatives(self):
-    #     self.pd.set_positions(np.array([[0.0, 0.0],[1.0, 0.0]]))
-    #     self.pd.set_weights(np.array([1.0, 2.0]))
-    #     check_ders( self, self.pd )
+    def test_derivatives(self):
+        self.pd.set_positions(np.array([[0.0, 0.0],[1.0, 0.0]]))
+        self.pd.set_weights(np.array([1.0, 2.0]))
+        check_ders( self, self.pd )
 
 
 if __name__ == '__main__':
